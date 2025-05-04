5c49d7feaa6c688ba3daf85d03328e52a068cef6
Adikac01 <adikac919@gmail.com>
2025-05-04 23:34:48 +0000

    Fix broken endpoints

--- src/main/kotlin/backend/conference_app/bookmark/BookmarkController.kt
+++ src/main/kotlin/backend/conference_app/bookmark/BookmarkController.kt
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/protected/bookmarks")
+@RequestMapping("/api/protected/bookmarks")
 class BookmarkController(private val bookmarkService: BookmarkService) {
 
 	@GetMapping("/{userId}")
